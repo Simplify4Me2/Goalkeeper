6a10fef8f823fb55a5ecab8fdb87ce56b71a609d
Simon <simon.vereecke@live.com>
2020-04-24 23:03:38 +0200

    icon color

--- goal-keeper/src/footer/Footer.js
+++ goal-keeper/src/footer/Footer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Footer.css';
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faTwitter, faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons";
-// import { face } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
@@ -13,10 +12,10 @@ class Footer extends React.Component {
             <section className="social">
                 <footer>
                     <ul className="icons">
-                        <li><a href="http://localhost:3000/" className="icon brands alt"><FontAwesomeIcon icon={faTwitter} /><span className="label">Twitter</span></a></li>
-                        <li><a href="http://localhost:3000/" className="icon brands alt"><FontAwesomeIcon icon={faFacebookF} /><span className="label">Facebook</span></a></li>
-                        <li><a href="http://localhost:3000/" className="icon brands alt"><FontAwesomeIcon icon={faInstagram} /><span className="label">Instagram</span></a></li>
-                        <li><a href="http://localhost:3000/" className="icon solid alt"><FontAwesomeIcon icon={faEnvelope} /><span className="label">Email</span></a></li>
+                        <li><a href="http://localhost:3000/" className="icon brands alt"><FontAwesomeIcon style={{color:"#C0C0C0"}} icon={faTwitter} /><span className="label">Twitter</span></a></li>
+                        <li><a href="http://localhost:3000/" className="icon brands alt"><FontAwesomeIcon style={{color:"#C0C0C0"}} icon={faFacebookF} /><span className="label">Facebook</span></a></li>
+                        <li><a href="http://localhost:3000/" className="icon brands alt"><FontAwesomeIcon style={{color:"#C0C0C0"}} icon={faInstagram} /><span className="label">Instagram</span></a></li>
+                        <li><a href="http://localhost:3000/" className="icon solid alt"><FontAwesomeIcon style={{color:"#C0C0C0"}} icon={faEnvelope} /><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
                         <li>&copy; Simon</li><li>Design: <a href="http://localhost:3000/">Simon</a></li>
