9beca1580be564717683328ada59accd08b536b1
Simon <simon.vereecke@live.com>
2020-05-30 00:26:46 +0200

    CI test

---
+++ goal-keeper/src/footer/Footer.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Footer from './Footer';
+
+test('renders Copyright', () => {
+    const { getByText } = render(<Footer />);
+    const linkElement = getByText(/© Simon/i)
+    expect(linkElement).toBeInTheDocument();
+})
