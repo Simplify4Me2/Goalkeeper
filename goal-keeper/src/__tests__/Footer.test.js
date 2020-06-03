4124d2829bc1ab4c1d5c4a8afc05572ed2100956
Simon <simon.vereecke@live.com>
2020-06-03 22:55:44 +0200

    write build info

--- goal-keeper/src/__tests__/Footer.test.js
+++ goal-keeper/src/__tests__/Footer.test.js
@@ -4,7 +4,7 @@ import Footer from '../footer/Footer';
 
 it('renders Copyright', () => {
     const { getByText } = render(<Footer />);
-    const linkElement = getByText(/© Simonito/i)
+    const linkElement = getByText(/© Simon/i)
     expect(linkElement).toBeInTheDocument();
     // const div = document.createElement('div');
 
