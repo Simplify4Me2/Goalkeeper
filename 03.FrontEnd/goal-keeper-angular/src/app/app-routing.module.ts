e03014d2626807d2ca807b712c6765ef6f198e5a
Simon <simon.vereecke@live.com>
2020-11-10 12:09:11 +0100

    vulnerability patch + wildcard routing

--- 03.FrontEnd/goal-keeper-angular/src/app/app-routing.module.ts
+++ 03.FrontEnd/goal-keeper-angular/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'home', component: HomeComponent }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
