b66b16a3e06d81391cdad54542f6ed257cf51605
Simplify4Me2 <simon.vereecke@live.com>
2020-11-15 12:44:23 +0100

    remove jasmine

--- 03.FrontEnd/goal-keeper-angular/src/app/home/components/fixture/fixture.component.spec.ts
+++ 03.FrontEnd/goal-keeper-angular/src/app/home/components/fixture/fixture.component.spec.ts
@@ -19,7 +19,7 @@ describe('FixtureComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  xit('should create', () => {
     expect(component).toBeTruthy();
   });
 });
