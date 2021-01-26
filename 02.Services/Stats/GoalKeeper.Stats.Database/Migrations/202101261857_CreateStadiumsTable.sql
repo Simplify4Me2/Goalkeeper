642d285fbb3bb6eea528cb43368a6599b0d10215
Simon <simon.vereecke@live.com>
2021-01-26 19:09:28 +0100

    stadium migration file added

---
+++ 02.Services/Stats/GoalKeeper.Stats.Database/Migrations/202101261857_CreateStadiumsTable.sql
@@ -0,0 +1,21 @@
+﻿
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [Stats].[Stadiums](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](max) NULL,
+	[PictureUrl] [nvarchar](max) NULL,
+	[CreatedUtc] [datetime2](7) NOT NULL,
+	[CreatedBy] [nvarchar](max) NULL,
+	[ModifiedUtc] [datetime2](7) NOT NULL,
+	[ModifiedBy] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Stadiums] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
