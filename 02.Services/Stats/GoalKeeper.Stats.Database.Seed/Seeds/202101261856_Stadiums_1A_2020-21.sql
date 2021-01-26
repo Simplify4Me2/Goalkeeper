3b184434ddc724a0bd1cf61d756cc6632f6baa8e
Simon <simon.vereecke@live.com>
2021-01-26 21:23:43 +0100

    updated snapshots

--- 02.Services/Stats/GoalKeeper.Stats.Database.Seed/Seeds/202101261856_Stadiums_1A_2020-21.sql
+++ 02.Services/Stats/GoalKeeper.Stats.Database.Seed/Seeds/202101261856_Stadiums_1A_2020-21.sql
@@ -1,1 +1,5 @@
-﻿
+﻿INSERT INTO [Stats].Stadiums
+([Name], [PictureUrl], CreatedUtc, CreatedBy, ModifiedUtc, ModifiedBy)
+VALUES
+  ('Jan Breydelstadion', '', GETDATE(), 'Seed', GETDATE(), 'Seed')
+, ('Bosuilstadion', '', GETDATE(), 'Seed', GETDATE(), 'Seed') 
