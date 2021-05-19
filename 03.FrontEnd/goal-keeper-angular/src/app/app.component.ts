060b57e111abe6fcaa341653e33ac717c4034221
Simplify4Me2 <simon.vereecke@live.com>
2021-05-19 22:31:14 +0200

    restyle ranking table

--- 03.FrontEnd/goal-keeper-angular/src/app/app.component.ts
+++ 03.FrontEnd/goal-keeper-angular/src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template:  `<app-header></app-header>
+              <div class="content">
                 <router-outlet></router-outlet>
+              </div>
               <app-footer></app-footer>`,
   styleUrls: ['./app.component.sass']
 })
