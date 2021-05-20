7afe4a9da048b606653519f73af50bf71b071743
Simon <simon.vereecke@live.com>
2021-05-20 23:28:58 +0200

    add CreateMatchesTable migration

---
+++ 02.Services/Stats/GoalKeeper.Stats.Database/Migrations/202105202313_CreateMatchesTable.sql
@@ -0,0 +1,35 @@
+﻿
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [Stats].[Matches](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[HomeTeamId] [bigint] NOT NULL,
+	[HomeTeamScore] [int] NOT NULL,
+	[AwayTeamId] [bigint] NOT NULL,
+	[AwayTeamScore] [int] NOT NULL,
+	[Matchday] [int] NOT NULL,
+	[DateUtc] [datetime2](7) NOT NULL,
+	[CreatedUtc] [datetime2](7) NOT NULL,
+	[CreatedBy] [nvarchar](max) NULL,
+	[ModifiedUtc] [datetime2](7) NOT NULL,
+	[ModifiedBy] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Stadiums] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [Stats].[Matches] 
+ADD CONSTRAINT [FK_Matches_Teams] FOREIGN KEY ([HomeTeamId]) REFERENCES [Stats].[Teams] ([Id])
+
+GO 
+
+ALTER TABLE [Stats].[Matches] 
+ADD CONSTRAINT [FK_Matches_Teams] FOREIGN KEY ([AwayTeamId]) REFERENCES [Stats].[Teams] ([Id])
+
+GO
