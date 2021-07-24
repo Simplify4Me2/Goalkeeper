b0c2004872a2c127f9a569c3432d2fc895676aa4
Simon <simon.vereecke@live.com>
2021-07-24 22:13:00 +0200

    Add console app to collect scores

--- 02.Services/DataCollector/DataCollector/UnitTest1.cs
+++ 02.Services/DataCollector/DataCollector/UnitTest1.cs
@@ -2,6 +2,7 @@ using System;
 using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Linq;
 
 namespace DataCollector
 {
@@ -21,8 +22,15 @@ namespace DataCollector
             //inpoutElement.Click();
 
             var section = driver.FindElement(By.Id("sc-matchdays-uuid-matchdays"));
+            var children = section.FindElements(By.XPath(".//li"));
+
+            var firstLiItemSpan = children.FirstOrDefault();
+            var homeTeamName = firstLiItemSpan.FindElement(By.XPath("//span[contains(@class,'sc-scoreboard__team--home')]"));
 
             Assert.NotNull(section);
+            Assert.NotNull(children);
+
+            Assert.Equal("Standard", homeTeamName.Text.Trim());
 
             driver.Close();
         }
---
+++ 02.Services/DataCollector/JPLScore/Program.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace JPLScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://sporza.be/nl/categorie/voetbal/jupiler-pro-league/";
+
+            var section = driver.FindElement(By.Id("sc-matchdays-uuid-matchdays"));
+            var children = section.FindElements(By.XPath(".//li"));
+
+            foreach (var item in children)
+            {
+                IWebElement homeTeamNameElement = null;
+                IWebElement awayTeamNameElement = null;
+                IWebElement homeTeamScoreElement = null;
+                IWebElement awayTeamScoreElement = null;
+                try
+                {
+                    homeTeamNameElement = item.FindElement(By.XPath(".//span[contains(@class,'sc-scoreboard__team--home')]"));
+
+                    awayTeamNameElement = item.FindElement(By.XPath(".//span[contains(@class,'sc-scoreboard__team--away')]"));
+
+                    homeTeamScoreElement = item.FindElement(By.XPath(".//span[contains(@class,'sc-score__home')]"));
+                    awayTeamScoreElement = item.FindElement(By.XPath(".//span[contains(@class,'sc-score__away')]"));
+                }
+                catch (NoSuchElementException)
+                {
+                    Console.WriteLine($"{homeTeamNameElement.Text} - {awayTeamNameElement.Text} : not played yet.");
+                }
+
+                if (homeTeamNameElement != null && awayTeamNameElement != null && homeTeamScoreElement != null && awayTeamScoreElement != null)
+                {
+                    var homeTeamScore = int.Parse(homeTeamScoreElement.Text);
+                    var awayTeamScore = int.Parse(awayTeamScoreElement.Text);
+
+                    Console.WriteLine($"{homeTeamNameElement.Text} {homeTeamScore} - {awayTeamScore} {awayTeamNameElement.Text}");
+                }
+            }
+
+            driver.Close();
+        }
+    }
+}
