74e7f665e23f68b4c0c224cd7cfad58b1859cfe2
Simon <simon.vereecke@live.com>
2021-07-25 22:32:02 +0200

    Promovendi teams and stadiums seed

---
+++ 02.Services/Stats/GoalKeeper.Stats.Database.Seed/Seeds/202107252210_PromovendiStadiums_1A_2021-22.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO [Stats].Stadiums
+([Name], [PictureUrl], CreatedUtc, CreatedBy, ModifiedUtc, ModifiedBy)
+VALUES
+  ('Joseph Marien Stadion', '', GETDATE(), 'Seed', GETDATE(), 'Seed')
+, ('Pairaystadion', '', GETDATE(), 'Seed', GETDATE(), 'Seed') 
---
+++ 02.Services/Stats/GoalKeeper.Stats.Database.Seed/Seeds/202107252221_PromovendiTeams_1A_2021-22.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO Stats.Teams
+([Name], [StadiumId], CreatedUtc, CreatedBy, ModifiedUtc, ModifiedBy)
+VALUES
+  ('Union', (SELECT Id FROM [Stats].Stadiums WHERE [Name] like '%Bosuilstadion%'), GETDATE(), 'Seed', GETDATE(), 'Seed')
+, ('RFC Seraing', (SELECT Id FROM [Stats].Stadiums WHERE [Name] like '%Pairaystadion%'), GETDATE(), 'Seed', GETDATE(), 'Seed') 
