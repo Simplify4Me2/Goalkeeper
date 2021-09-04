3d05a8aee8acc85721af7a429b150c24536cf182
Simon <simon.vereecke@live.com>
2021-09-04 12:12:26 +0200

    A working integration test. but does it work the right way?

---
+++ 02.Services/Stats/GoalKeeper.Stats.IntegrationTests/ApiWebApplicationFactory.cs
@@ -0,0 +1,57 @@
+﻿using GoalKeeper.Stats.Application.Ports;
+using GoalKeeper.Stats.Domain.ValueObjects;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace GoalKeeper.Stats.IntegrationTests
+{
+    public class ApiWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        public IConfiguration Configuration { get; private set; }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(config =>
+            {
+                Configuration = new ConfigurationBuilder().AddJsonFile("integrationsettings.json").Build();
+
+                config.AddConfiguration(Configuration);
+            });
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddTransient<IMatchRepository, MatchRepositoryStub>();
+            });
+        }
+    }
+
+    public class MatchRepositoryStub : IMatchRepository
+    {
+        public Task<IEnumerable<Fixture>> GetFixtures(int matchday, CancellationToken cancellationToken)
+        {
+            var returnValue = new List<Fixture>
+            {
+                new Fixture(1, new (1, "Team 1", new(1, "Test stadium 1"), new List<Player> { }), new(2, "Team 2", new(2, "Test Stadium 2"), new List<Player> {}), new DateTime(2021, 09, 03), 1),
+            };
+            return Task.FromResult(returnValue.Select(fixture => fixture));
+        }
+
+        public Task<IEnumerable<PlayedMatch>> GetResults(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Save(PlayedMatch match, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- 02.Services/Stats/GoalKeeper.Stats.IntegrationTests/TeamTests.cs
+++ 02.Services/Stats/GoalKeeper.Stats.IntegrationTests/TeamTests.cs
@@ -1,5 +1,13 @@
+using FluentAssertions;
+using GoalKeeper.Common.Application.IO;
+using GoalKeeper.Stats.Application.IO.DTOs;
 using GoalKeeper.Stats.Application.IO.Services;
+using GoalKeeper.Stats.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Moq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,12 +15,15 @@ using Xunit;
 
 namespace GoalKeeper.Stats.IntegrationTests
 {
-    public class TeamTests
+    // https://timdeschryver.dev/blog/how-to-test-your-csharp-web-api
+    public class TeamTests : IClassFixture<WebApplicationFactory<TeamController>>
     {
-        private readonly TeamService _service;
+        //private readonly TeamService _service;
+        readonly HttpClient _client;
 
-        public TeamTests()
-{
+        public TeamTests(WebApplicationFactory<TeamController> fixture)
+        {
+            _client = fixture.CreateClient();
             //var clientFactory = new Mock<IHttpClientFactory>();
             //clientFactory.Setup(x => x.CreateClient("auToHttpClient"))
             //             .Returns(() => TokenHelper.GetAuthenticatedClient());
@@ -25,7 +36,25 @@ namespace GoalKeeper.Stats.IntegrationTests
         [Fact]
         public async Task Test1()
         {
-            await _service.GetTeam("Test", CancellationToken.None);
+            //await _service.GetTeam("Test", CancellationToken.None);
+
+            var response = await _client.GetAsync("api/team");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var teams = JsonConvert.DeserializeObject<Result<IEnumerable<TeamDTO>>>(await response.Content.ReadAsStringAsync());
+            teams.Data.Should().HaveCount(18);
+        }
+
+        [Fact]
+        public async Task Test2()
+        {
+            //await _service.GetTeam("Test", CancellationToken.None);
+
+            var response = await _client.GetAsync("api/team");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var teams = JsonConvert.DeserializeObject<Result<IEnumerable<TeamDTO>>>(await response.Content.ReadAsStringAsync());
+            teams.Data.Should().HaveCount(18);
         }
     }
 }
