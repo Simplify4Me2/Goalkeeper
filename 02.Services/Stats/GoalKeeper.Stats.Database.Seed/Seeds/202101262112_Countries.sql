b50ad62a0c98bbc60527bbe817e7793896758bda
Simon <simon.vereecke@live.com>
2021-09-30 21:57:49 +0200

    more countries seed

--- 02.Services/Stats/GoalKeeper.Stats.Database.Seed/Seeds/202101262112_Countries.sql
+++ 02.Services/Stats/GoalKeeper.Stats.Database.Seed/Seeds/202101262112_Countries.sql
@@ -1,6 +1,16 @@
 INSERT INTO dbo.Countries
 (TwoLetterISOCode, [Name], CreatedUtc, CreatedBy, ModifiedUtc, ModifiedBy)
 VALUES
-  ('BE', 'Belgium',		GETDATE(), 'Seed', GETDATE(), 'Seed'),
-  ('PT', 'Portugal',	GETDATE(), 'Seed', GETDATE(), 'Seed'),
-  ('SN', 'Senegal',		GETDATE(), 'Seed', GETDATE(), 'Seed')
+  ('BE', 'Belgium',			GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('CM', 'Cameroon',		GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('CO', 'Colombia',		GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('HR', 'Croatia',			GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('DK', 'Denmark',			GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('FR', 'France',			GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('JP', 'Japan',			GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('PT', 'Portugal',		GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('SN', 'Senegal',			GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('CH', 'Scotland',		GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('CH', 'Switzerland',		GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('UA', 'Ukraine',			GETDATE(), 'Seed', GETDATE(), 'Seed'),
+  ('GB', 'United Kingdom',	GETDATE(), 'Seed', GETDATE(), 'Seed')
