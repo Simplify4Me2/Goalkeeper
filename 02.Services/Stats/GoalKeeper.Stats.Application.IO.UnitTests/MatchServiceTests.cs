0827dc101d2688b50397bdf6f46129047cd43832
Simon <simon.vereecke@live.com>
2021-11-16 10:11:54 +0100

    MatchService test for throwing error on fail

--- 02.Services/Stats/GoalKeeper.Stats.Application.IO.UnitTests/MatchServiceTests.cs
+++ 02.Services/Stats/GoalKeeper.Stats.Application.IO.UnitTests/MatchServiceTests.cs
@@ -5,6 +5,7 @@ using GoalKeeper.Stats.Application.IO.Services;
 using Newtonsoft.Json;
 using RichardSzalay.MockHttp;
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -29,6 +30,24 @@ namespace GoalKeeper.Stats.Application.IO.UnitTests
 
             // Assert
             response.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public async Task AllMatches_Error_ThrowsError()
+        {
+            // Arrange
+            int matchday = 3;
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.When("*").Respond(handler => new HttpResponseMessage(HttpStatusCode.InternalServerError));
+            var client = mockHttp.ToHttpClient();
+
+            var service = new MatchService(client);
+
+            // Act
+            var action = async () => await service.AllMatches(matchday, CancellationToken.None);
+
+            // Assert
+            await action.Should().ThrowAsync<Exception>();
         }
 
         private static HttpClient CreateMockHttpClient<TResponse>(Result<TResponse> expectedResponse, string url)
