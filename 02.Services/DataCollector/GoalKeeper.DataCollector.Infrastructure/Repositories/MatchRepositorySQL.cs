f43f45736eb4baf04b566ddecbd29792ebf53b8f
Simon <simon.vereecke@live.com>
2021-11-22 22:17:52 +0100

    connect interface in Infrastructure project

---
+++ 02.Services/DataCollector/GoalKeeper.DataCollector.Infrastructure/Repositories/MatchRepositorySQL.cs
@@ -0,0 +1,20 @@
+﻿using GoalKeeper.DataCollector.Application.Ports;
+using GoalKeeper.DataCollector.Domain;
+using System;
+using System.Threading.Tasks;
+
+namespace GoalKeeper.DataCollector.Infrastructure.Repositories
+{
+    public class MatchRepositorySQL : IMatchRepository
+    {
+        public Task<Match[]> Get(int matchday)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save(Match[] matches)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 02.Services/DataCollector/GoalKeeper.DataCollector.Infrastructure/WebScrapers/MatchWebScraperSelenium.cs
@@ -0,0 +1,15 @@
+﻿using GoalKeeper.DataCollector.Application.Ports;
+using GoalKeeper.DataCollector.Domain;
+using System;
+using System.Threading.Tasks;
+
+namespace GoalKeeper.DataCollector.Infrastructure.WebScrapers
+{
+    public class MatchWebScraperSelenium : IMatchWebScraper
+    {
+        public Task<Match[]> Get(int matchday)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
