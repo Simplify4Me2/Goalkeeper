068571d999159bea5303d962db54d6fb11b6960d
Simon <simon.vereecke@live.com>
2026-05-25 21:01:33 +0200

    replace sql server with postgresql in server

--- Goalkeeper.Server/Infrastructure/Data/Migrations/20260525184902_InitialCreate.cs
+++ Goalkeeper.Server/Infrastructure/Data/Migrations/20260525184902_InitialCreate.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
 namespace Goalkeeper.Server.Infrastructure.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreateTeams : Migration
+    public partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -14,9 +15,9 @@ namespace Goalkeeper.Server.Infrastructure.Data.Migrations
                 name: "Teams",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
                 },
                 constraints: table =>
                 {
