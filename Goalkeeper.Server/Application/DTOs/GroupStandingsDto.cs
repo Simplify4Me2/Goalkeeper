96f48e29ff5db0043b818299c4845aeaf7f5e5be
Simon <simon.vereecke@live.com>
2026-05-31 21:53:02 +0200

    Create the standings DTOs

---
+++ Goalkeeper.Server/Application/DTOs/GroupStandingsDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Goalkeeper.Server.Application.DTOs;
+
+public class GroupStandingsDto
+{
+    public string GroupName { get; set; } = string.Empty;
+    public IEnumerable<TeamStandingDto> Standings { get; set; } = [];
+}
