bdc50532c765df3f8a23b5d907b3da51c80be5bc
Simon <simon.vereecke@live.com>
2026-08-31 22:16:35 +0200

    update migration to add flag

--- Goalkeeper.Server/Infrastructure/Data/Migrations/20260831201113_Followup.Designer.cs
+++ Goalkeeper.Server/Infrastructure/Data/Migrations/20260831201113_Followup.Designer.cs
@@ -2,6 +2,7 @@
 using Goalkeeper.Server.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Goalkeeper.Server.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(GoalkeeperDbContext))]
-    partial class GoalkeeperDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260831201113_Followup")]
+    partial class Followup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Goalkeeper.Server/Infrastructure/Data/Migrations/20260831201113_Followup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Goalkeeper.Server.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Followup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Flag",
+                table: "Teams",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Flag",
+                table: "Teams");
+        }
+    }
+}
